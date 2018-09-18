cf05f7600ed7b444984611c4856559d368547afb
EsdrasXavier <esdrasxa@gmail.com>
2018-09-18 20:40:59 -0300

    Finalizando projeto de teste de software. Ainda há necessidade de verificar se está tudo correto

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# selenium-test
-Projeto para aula de teste de software feito com selenium webdriver
+# Selenium teste
+
+Um projeto criado para matéria de teste de software. Este projeto visa utilizar da API Selenium Webdriver em java e fazer um teste totalmente automatizado em um site. O site utilizado como objetivo de teste foi o da própria instituição: [Católica de Santa Catarina](http://www.catolicasc.org.br/)
+
+# Dependências
+Para poder utilizar deste programa de teste feito deve-se possuir conexão a internet e o driver do navegador baixado e específicado o caminho no código.
+Neste exemplo foi utilizado do Google Chrome porém existe a possiblidade de utilizar em qualquer navegado.
+Para baixar o driver do google acesse: http://chromedriver.chromium.org/downloads
+Para especificar o caminho altere a variável pathToDriver:
+```
+// Configura o caminho para o driver desejado
+String pathToDriver = "Caminho para o seu driver";
+```
+
+# Rodando
+Após baixar e configurar as dependências basta rodar o programa que ele irá fazer um teste de envio de formulário no site e retornar se foi possível enviar ou não.
--- seleniumtest/src/main/java/Initializer.java
+++ seleniumtest/src/main/java/Initializer.java
@@ -1,64 +1,103 @@
 //package br.org.catolicasc.test;
 package org.openqa.selenium.example;
 
-import jdk.jfr.events.ExceptionThrownEvent;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Initializer{
-    public static void main(String[] args) throws InterruptedException {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
 
+    public static void main(String[] args) {
 
-            System.setProperty("webdriver.chrome.driver", "C:\\BATATA\\chromedriver.exe");
-            WebDriver driver = new ChromeDriver();
+        // Configura o caminho para o driver desejado
+        String pathToDriver = "C:\\Users\\esdra\\Downloads\\chromedriver.exe";
 
-            driver.get("http://www.catolicasc.org.br/");
+        // Configura o driver a ser usado, no caso do google chrome
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
 
-            System.out.println("O título da página é: " + driver.getTitle());
+        // Cria nova instância do chrome
+        WebDriver driver = new ChromeDriver();
 
-            WebElement joi = driver.findElement(By.className("joi"));
-            joi.click();
+        // Aponta para o site que ele deve acessar
+        driver.get("http://www.catolicasc.org.br/");
 
-            WebElement fullName = driver.findElement(By.id("nome"));
-            fullName.sendKeys("Teste de teste");
+        // Diz qual o título da página
+        System.out.println("O título da página é: " + driver.getTitle());
 
-            WebElement phoneNumber = driver.findElement(By.className("phoneNumber"));
-            phoneNumber.sendKeys("40028922");
+        // Novo elemento para o campus de jaraguá, e depois clicar nele
+        WebElement joi = driver.findElement(By.className("joi"));
+        joi.click();
 
-            WebElement email = driver.findElement(By.id("email"));
-            email.sendKeys("teste@catolicasc.org.br");
+        // Novo elemento para o campo que preenche o nome e depois preenche-lo
+        WebElement fullName = driver.findElement(By.id("nome"));
+        fullName.sendKeys("Teste de teste");
 
-            WebElement selection = driver.findElement(By.id("catolica_tipo_de_ensino"));
-            selection.sendKeys("1");
+        // Novo elemento para o número de telefone e preenche-lo
+        WebElement phoneNumber = driver.findElement(By.className("phoneNumber"));
+        phoneNumber.sendKeys("40028922");
 
-            WebElement item = driver.findElement(By.xpath("//div[@data-value='1']"));
-            item.click();
+        // Novo elemento para o email e preenche-lo
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("teste@catolicasc.org.br");
 
-            WebElement text = driver.findElement(By.name("cad_mensagem"));
-            text.sendKeys("isso é um teste de teste pra enviar um texto para verificar se esse teste tá funcionando");
+        // Novo elemento para selecionar o tipo do curso
+        WebElement selection = driver.findElement(By.id("catolica_tipo_de_ensino"));
 
+        // Nova instância utilizada para simular um hover sobre o elemento
+        Actions builder = new Actions(driver);
+        try {
+            builder.moveToElement(selection).build().perform();
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-            WebElement send = driver.findElement(By.className("wpcf7-submit"));
-            send.click();
+        // Novo elemento para selecionar o curso
+        WebElement item = driver.findElement(By.xpath("//div[@data-value='1']"));
+        item.click();
 
-            WebElement teste = driver.findElement(By.id("mensagem-contato"));
+        try { // Remove hover do tipo de interesse
+            builder.moveToElement(email).build().perform();
+            Thread.sleep(500);
+        } catch (Exception e) {
+            System.out.println(e);
+            driver.quit();
+            return ;
+        }
 
-            while(teste.getText() == ""){
-                System.out.println(teste.getText());
-            }
+        // Novo elemento para mensagem a ser mandada
+        WebElement text = driver.findElement(By.name("cad_mensagem"));
+        text.sendKeys("isso é um teste de teste pra enviar um texto para verificar se esse teste tá funcionando");
 
+        // Novo elemento para envio do formulário
+        WebElement send = driver.findElement(By.className("wpcf7-submit"));
+        send.click();
 
+        try {
+            (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+                public Boolean apply(WebDriver d) {
+                    return d.findElement(By.id("mensagem-contato")).getText().length() != 0;
+                }
+            });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-        Thread.sleep(500000);
+        WebElement callback = driver.findElement(By.id("mensagem-contato"));;
+        String callbackText = callback.getText();
 
-        //Close the browser
+        if (callbackText.equals("Seus dados foram enviados!")) {
+            System.out.println("Os dados foram enviados com sucesso!");
+        } else {
+            throw new java.lang.RuntimeException("Erro ao finalizar formulário.");
+        }
+
+        // Fecha o drivers
         driver.quit();
-
-
     }
 }
