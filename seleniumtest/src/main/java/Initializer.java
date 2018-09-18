19e8e0dc4b3e8e02ad6910327906f1684a6d5d08
Hu3diger <30418918+Hu3diger@users.noreply.github.com>
2018-09-17 22:01:37 -0300

    teste

--- seleniumtest/src/main/java/Initializer.java
+++ seleniumtest/src/main/java/Initializer.java
@@ -6,39 +6,55 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Initializer {
-    public static void main(String[] args) {
+public class Initializer{
+    public static void main(String[] args) throws InterruptedException {
         // Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface,
         // not the implementation.
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\esdra\\Downloads\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
 
-        // And now use this to visit Google
-        driver.get("http://www.catolicasc.org.br/jaragua-do-sul/");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+            System.setProperty("webdriver.chrome.driver", "C:\\BATATA\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
 
-        // Find the text input element by its name
-        WebElement completeName = driver.findElement(By.id("nome"));
-        completeName.sendKeys("teste batata");
+            driver.get("http://www.catolicasc.org.br/");
 
-        System.out.println("Page title is: " + driver.getTitle());
+            System.out.println("O título da página é: " + driver.getTitle());
 
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("teste é ruim");
+            WebElement joi = driver.findElement(By.className("joi"));
+            joi.click();
+
+            WebElement fullName = driver.findElement(By.id("nome"));
+            fullName.sendKeys("Teste de teste");
+
+            WebElement phoneNumber = driver.findElement(By.className("phoneNumber"));
+            phoneNumber.sendKeys("40028922");
+
+            WebElement email = driver.findElement(By.id("email"));
+            email.sendKeys("teste@catolicasc.org.br");
+
+            WebElement selection = driver.findElement(By.id("catolica_tipo_de_ensino"));
+            selection.sendKeys("1");
+
+            WebElement item = driver.findElement(By.xpath("//div[@data-value='1']"));
+            item.click();
+
+            WebElement text = driver.findElement(By.name("cad_mensagem"));
+            text.sendKeys("isso é um teste de teste pra enviar um texto para verificar se esse teste tá funcionando");
+
+
+            WebElement send = driver.findElement(By.className("wpcf7-submit"));
+            send.click();
+
+            WebElement teste = driver.findElement(By.id("mensagem-contato"));
+
+            while(teste.getText() == ""){
+                System.out.println(teste.getText());
             }
-        });
 
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
+
+
+        Thread.sleep(500000);
 
         //Close the browser
         driver.quit();
